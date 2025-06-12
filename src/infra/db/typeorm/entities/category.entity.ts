30edce2530120e1f32f0197577c45d63b08bbc41
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-06-12 00:38:01 -0300

    refactor: linting

--- src/infra/db/typeorm/entities/category.entity.ts
+++ src/infra/db/typeorm/entities/category.entity.ts
@@ -1,44 +1,44 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
-    ManyToOne,
-    JoinColumn,
-    OneToMany,
-} from 'typeorm';
-import { CategoryType } from '@domain/models/category.model';
-import { UserEntity } from './user.entity';
-import { EntryEntity } from './entry.entity';
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+} from "typeorm";
+import { CategoryType } from "@domain/models/category.model";
+import { UserEntity } from "./user.entity";
+import { EntryEntity } from "./entry.entity";
 
-@Entity('categories')
+@Entity("categories")
 export class CategoryEntity {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column()
   name: string;
 
   @Column({
-    type: 'enum',
-    enum: ['INCOME', 'EXPENSE'],
+    type: "enum",
+    enum: ["INCOME", "EXPENSE"],
   })
   type: CategoryType;
 
-  @Column({ name: 'user_id' })
+  @Column({ name: "user_id" })
   userId: string;
 
   @ManyToOne(() => UserEntity, (user) => user.categories)
-  @JoinColumn({ name: 'user_id' })
+  @JoinColumn({ name: "user_id" })
   user: UserEntity;
 
   @OneToMany(() => EntryEntity, (entry) => entry.category)
   entries: EntryEntity[];
 
-  @CreateDateColumn({ name: 'created_at' })
+  @CreateDateColumn({ name: "created_at" })
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn({ name: "updated_at" })
   updatedAt: Date;
-} 
+}
--- src/infra/db/typeorm/entities/user.entity.ts
+++ src/infra/db/typeorm/entities/user.entity.ts
@@ -1,17 +1,17 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-    UpdateDateColumn,
-    OneToMany,
-} from 'typeorm';
-import { EntryEntity } from './entry.entity';
-import { CategoryEntity } from './category.entity';
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from "typeorm";
+import { EntryEntity } from "./entry.entity";
+import { CategoryEntity } from "./category.entity";
 
-@Entity('users')
+@Entity("users")
 export class UserEntity {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column()
@@ -23,7 +23,7 @@ export class UserEntity {
   @Column({ nullable: true })
   password: string;
 
-  @Column({ name: 'avatar_url', nullable: true })
+  @Column({ name: "avatar_url", nullable: true })
   avatarUrl: string;
 
   @OneToMany(() => EntryEntity, (entry) => entry.user)
@@ -32,9 +32,9 @@ export class UserEntity {
   @OneToMany(() => CategoryEntity, (category) => category.user)
   categories: CategoryEntity[];
 
-  @CreateDateColumn({ name: 'created_at' })
+  @CreateDateColumn({ name: "created_at" })
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn({ name: "updated_at" })
   updatedAt: Date;
-} 
+}
