5a6df212d2c01d847611dd267fd922e330467b98
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-11-23 17:14:29 -0300

    feat: enhance user login process with logging capabilities

--- src/main/factories/usecases/auth/make-login-user.factory.ts
+++ src/main/factories/usecases/auth/make-login-user.factory.ts
@@ -1,10 +1,12 @@
 import type { TokenGenerator, UserRepository, Hasher } from '@/data/protocols';
 import { DbLoginUserUseCase } from '@/data/usecases';
+import type { ContextAwareLoggerService } from '@/infra/logging/context-aware-logger.service';
 
 export const makeLoginUserFactory = (
   userRepository: UserRepository,
   hasher: Hasher,
   tokenGenerator: TokenGenerator,
+  logger: ContextAwareLoggerService,
 ) => {
-  return new DbLoginUserUseCase(userRepository, hasher, tokenGenerator);
+  return new DbLoginUserUseCase(userRepository, hasher, tokenGenerator, logger);
 };
