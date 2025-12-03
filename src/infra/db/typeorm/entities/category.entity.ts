42a5ac4e7616d10632ed93b7abb9942defb400b8
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-12-03 18:50:04 -0300

    feat: add soft delete functionality to CategoryEntity and update initial schema migration

--- src/infra/db/typeorm/entities/category.entity.ts
+++ src/infra/db/typeorm/entities/category.entity.ts
@@ -1,6 +1,7 @@
 import {
   Column,
   CreateDateColumn,
+  DeleteDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
@@ -53,4 +54,7 @@ export class CategoryEntity {
 
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date | null;
 }
--- src/infra/db/typeorm/migrations/1733087000000-InitialSchema.ts
+++ src/infra/db/typeorm/migrations/1733087000000-InitialSchema.ts
@@ -188,6 +188,11 @@ export class InitialSchema1733087000000 implements MigrationInterface {
             type: 'timestamp',
             default: 'now()',
           },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
         ],
         foreignKeys: [
           new TableForeignKey({
