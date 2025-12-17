b2157da57b57332c6abe30c792c1445110eedeff
Aleksander Sousa <aleksander-ssousa@outlook.com>
2025-12-16 21:03:00 -0300

    refactor: remove IdGenerator from DbAddEntryUseCase

--- src/main/factories/usecases/categories/make-list-categories.factory.ts
+++ src/main/factories/usecases/categories/make-list-categories.factory.ts
@@ -1,8 +1,10 @@
 import { DbListCategoriesUseCase } from '@data/usecases/db-list-categories.usecase';
 import { CategoryRepository } from '@/data/protocols/repositories/category-repository';
+import type { Logger } from '@/data/protocols';
 
 export const makeListCategoriesFactory = (
   categoryRepository: CategoryRepository,
+  logger: Logger,
 ): DbListCategoriesUseCase => {
-  return new DbListCategoriesUseCase(categoryRepository);
+  return new DbListCategoriesUseCase(categoryRepository, logger);
 };
